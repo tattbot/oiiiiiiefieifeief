16499d2a8b45cb62ebd47e8c8044ecbe0de70f20
tattbot <164650741+tattbot@users.noreply.github.com>
2024-03-23 19:26:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import requests
+import time
+
+api_key = '7077041742:AAGueuEPoethOJ2U2BFCbYVfyKVqYmBqX1k'
+channel_username = '@Ton_Pricee'
+
+def send_message(message):
+    url = f'https://api.telegram.org/bot{api_key}/sendMessage'
+    params = {
+        'chat_id': channel_username,
+        'text': message
+    }
+    response = requests.post(url, params=params)
+
+while True:
+    re = requests.get('https://coinyep.com/api/v1/?from=TON&to=USD&lang=ar&format=json').json()['price']
+    send_message(f'{re}$')
+    time.sleep(300) # 600 seconds = 5 minutes
