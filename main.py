c147b2fc526f0d8dddae3eedaf4befb3bf1485a4
tattbot <164650741+tattbot@users.noreply.github.com>
2024-03-24 14:24:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import requests
-try:
- import time
-except ModuleNotFoundError:
- import os
- os.system("pip install -r requirements.txt")
-
+from bs4 import BeautifulSoup
 import time
 
 api_key = '7077041742:AAGueuEPoethOJ2U2BFCbYVfyKVqYmBqX1k'
@@ -19,9 +14,12 @@ def send_message(message):
     response = requests.post(url, params=params)
 
 while True:
-    time.sleep(300) # 600 seconds = 5 minutes
-    text = requests.get('https://www.okx.com/ar/convert/ton-to-usd').text
-    dd =text.split('اليوم TON إلى USD هو')[1]
-    rr = dd.split('."}},{"@type"')[0]
-    send_message(f'{rr}')
-
+    time.sleep(300) # 600 seconds = 5 minute
+    rer = requests.get('https://www.okx.com/ar/convert/ton-to-usd').text
+    soup = BeautifulSoup(rer, 'html.parser')
+    change_container = soup.find('div', class_='index_changeContainer__x0mTX')
+    change_percentage = change_container.text.split('(')[1].strip(')')
+    price = soup.find('h2', class_='index_price__Yjfiz').text.strip()
+    
+    send_message(f'''Price: {price}
+The Change: {change_percentage}''')
